2eef732fcef68d6017d51280da65fb0fb3625953
GaneshCoder847 <ganeshhake847@gmail.com>
2026-02-02 15:01:49 +0530

    Day 22 – Added scroll reveal animations for sections

--- script.js
+++ script.js
@@ -49,5 +49,23 @@ resetBtn.addEventListener("click", function () {
 
     resetBtn.style.display = "none";
 });
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active");
+            }
+        });
+    },
+    {
+        threshold: 0.2
+    }
+);
+
+reveals.forEach(section => {
+    observer.observe(section);
+});
 
 
