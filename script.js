d86243cb8de9c972212758a6aa5d7d0d5934a29e
GaneshCoder847 <ganeshhake847@gmail.com>
2026-02-08 19:13:27 +0530

    Day 28: Added active navigation highlight using JavaScript

--- script.js
+++ script.js
@@ -105,5 +105,13 @@ window.addEventListener("scroll", () => {
         }
     });
 });
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.forEach(l => l.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
 
 
